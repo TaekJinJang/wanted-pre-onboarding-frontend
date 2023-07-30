5ad25eecc669806b073717dd134a48fc3e20dc2d
Taekjin Jang <xorwls1500@naver.com>
2023-07-30 12:40:57 +0900

    add :: 로그인 및 회원가입 input 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,6 +4,17 @@ function SignUp() {
   return (
     <>
       <div>원티드 프리온보딩 인턴십 사전과제</div>
+      <input
+        placeholder="example@email.com"
+        type="email"
+        data-testid="email-input"
+      />
+      <input
+        placeholder="암호를 입력해주세요."
+        type="password"
+        data-testid="password-input"
+      />
+      <button data-testid="signup-button" />
     </>
   );
 }
