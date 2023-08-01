c23c8a16c7b0fbf473a61573a5ae78231b8906a5
Taekjin Jang <xorwls1500@naver.com>
2023-08-01 15:29:01 +0900

    style :: onUpdate,Delete type 명시

--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -7,8 +7,38 @@ export type TodoListType = {
 
 export type TodoListPropsType = {
   todoList: TodoListType[];
+  onUpdateTodo: (
+    userToken: string | null,
+    id: number,
+    editTodo: string,
+    isCompleted: boolean
+  ) => Promise<void>;
+  onDeleteTodo: (userToken: string | null, todoId: number) => Promise<void>;
 };
 
 export type TodoInputType = {
   onCreateTodo: (userToken: string | null, userTodo: string) => void;
+};
+export type TodoType = {
+  todo: TodoListType;
+  onUpdateTodo: (
+    userToken: string | null,
+    id: number,
+    editTodo: string,
+    isCompleted: boolean
+  ) => Promise<void>;
+  onDeleteTodo: (userToken: string | null, todoId: number) => Promise<void>;
+};
+
+export type UpdateTodoType = {
+  serverTodo: string | undefined;
+  serverIsCompleted: boolean;
+  todoId: number;
+  setIsEditTodoState: (state: boolean) => void;
+  onUpdateTodo: (
+    userToken: string | null,
+    id: number,
+    editTodo: string,
+    isCompleted: boolean
+  ) => Promise<void>;
 };
