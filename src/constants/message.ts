5728689b9a3f99e901b6ec0ada75c9b51824522c
Taekjin Jang <xorwls1500@naver.com>
2023-08-23 23:32:17 +0900

    feat : message.ts -> ERROR_MESSAGE 추가

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -4,3 +4,4 @@ export const PASSWORD_VALIDATION_MSG = '비밀번호는 8자 이상입니다.';
 export const SIGININ_VALIDATION_MSG = '유효하지 않은 폼을 확인해 주세요.';
 export const SIGNUP_SUCCESS = '회원가입 성공';
 export const SIGNIN_SUCCESS = '로그인 성공';
+export const ERROR_MESSAGE = '잠시 후 다시 시도해주세요.';
