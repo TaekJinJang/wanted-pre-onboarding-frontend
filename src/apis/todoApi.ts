fb5f3bad1881e6f774ec8a632ef8caae0a54b3c7
Taekjin Jang <xorwls1500@naver.com>
2023-08-23 23:40:33 +0900

    feat : CRUD api -> try-catch문 추가

--- src/apis/todoApi.ts
+++ src/apis/todoApi.ts
@@ -1,26 +1,39 @@
 import * as api from './api';
 import API_KEY from '../constants/apiKey';
+import {ERROR_MESSAGE} from '../constants/message';
 
 export async function getTodos() {
     try {
         const response = await api.get(API_KEY, '/todos', true);
         return response;
     } catch {
-        alert('할 일을 가져오는데 실패했습니다.');
+        alert(ERROR_MESSAGE);
     }
 }
 
 export async function createTodo(todo: {todo: string}) {
-    const response = await api.post(API_KEY, '/todos', todo, true);
-    return response;
+    try {
+        const response = await api.post(API_KEY, '/todos', todo, true);
+        return response;
+    } catch {
+        alert(ERROR_MESSAGE);
+    }
 }
 
 export async function updateTodo(id: number, todo: {todo: string; isCompleted: boolean}) {
-    const response = await api.put(API_KEY, `/todos/${id}`, todo, true);
-    return response;
+    try {
+        const response = await api.put(API_KEY, `/todos/${id}`, todo, true);
+        return response;
+    } catch {
+        alert(ERROR_MESSAGE);
+    }
 }
 
 export async function deleteTodo(id: number) {
-    const response = await api.delete(API_KEY, `/todos/${id}`, true);
-    return response;
+    try {
+        const response = await api.delete(API_KEY, `/todos/${id}`, true);
+        return response;
+    } catch {
+        alert(ERROR_MESSAGE);
+    }
 }
