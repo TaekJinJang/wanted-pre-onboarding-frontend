0781b4a14b11126925d9609a831f6f14c2bc6d25
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 01:19:38 +0900

    type : AuthType 컴포넌트 분리 및 타입구분

--- src/apis/Auth.ts
+++ src/apis/Auth.ts
@@ -1,12 +1,9 @@
 import API_KEY from '../constants/apiKey';
+import { AuthRequestProps } from '../types/AuthTypes';
 import * as Api from './index';
 
 const AUTH_PATH = '/auth';
 
-interface AuthRequestProps {
-    email: string;
-    password: string;
-}
 
 export const postSignIn = async ({email, password}: AuthRequestProps) => {
     const response = await Api.post(API_KEY, `${AUTH_PATH}/signin`, {email, password}, false);
---
+++ src/types/AuthTypes.ts
@@ -0,0 +1,26 @@
+import {AxiosResponse} from 'axios'
+
+export type AuthAPI = ({email, password}: AuthRequestProps) => Promise<AxiosResponse>;
+
+export interface AuthPageProps {
+    api: AuthAPI;
+    title: string;
+    navigation: string;
+    link: string;
+    buttonType: string;
+    footerType: string;
+    testid: string;
+    footerText: string;
+    successMsg: string;
+    errorMsg: string;
+}
+
+export interface AuthFooterProps {
+    text: string;
+    type: string;
+    route: string;
+}
+export interface AuthRequestProps {
+    email: string;
+    password: string;
+}
