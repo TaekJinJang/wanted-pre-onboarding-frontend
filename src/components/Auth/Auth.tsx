593820b0442b083b4fbea4c892b1771f0ad16ba4
Taekjin Jang <xorwls1500@naver.com>
2023-08-27 23:49:59 +0900

    feat : 로그인 인풋 생성

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,4 +1,5 @@
 import * as S from '../../styles/Auth.styled';
+import Input from '../common/Input';
 
 const Auth = () => {
     return (
@@ -6,6 +7,24 @@ const Auth = () => {
             <S.AuthTitle>
                 <h1>회원가입</h1>
             </S.AuthTitle>
+            <form>
+                <div>
+                    <Input
+                        label='이메일'
+                        id='email'
+                        type='text'
+                        testid='email-input'
+                        placeholder='이메일을 입력해주세요.'
+                    />
+                    <Input
+                        label='비밀번호'
+                        id='password'
+                        type='password'
+                        testid='password-input'
+                        placeholder='비밀번호를 입력해주세요.'
+                    />
+                </div>
+            </form>
         </S.AuthContainer>
     );
 };
