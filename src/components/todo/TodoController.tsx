d6803d1d919e360300c350daf603b5e4952c070b
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 23:26:51 +0900

    feat : TodoController 작성 및 적용

--- src/components/todo/TodoController.tsx
+++ src/components/todo/TodoController.tsx
@@ -1,3 +1,35 @@
-const TodoController = () => {};
+import {useNavigate} from 'react-router-dom';
+import {useTodoDispatch} from '../../contexts/TodoContext';
+import {useCallback} from 'react';
+import * as TodoAPI from '../../apis/Todo';
+import ROUTES from '../../constants/routes';
+
+const TodoController = () => {
+    const navigate = useNavigate();
+    const todoDispatch = useTodoDispatch();
+
+    const getTodo = useCallback(async () => {
+        try {
+            const res = await TodoAPI.getTodos();
+            todoDispatch({type: 'GET', payload: res.data});
+        } catch (e: unknown) {
+            console.error('API호출 실패');
+            navigate(ROUTES.SIGNIN);
+        }
+    }, [todoDispatch]);
+
+    const createTodo = useCallback(
+        async (todo: string) => {
+            try {
+                const res = await TodoAPI.createTodo({todo: todo});
+                todoDispatch({type: 'CREATE', payload: res.data});
+            } catch (e: unknown) {
+                console.error('API호출 실패');
+            }
+        },
+        [todoDispatch]
+    );
+    return {getTodo, createTodo};
+};
 
 export default TodoController;
--- src/containers/TodoContainer.tsx
+++ src/containers/TodoContainer.tsx
@@ -1,17 +1,22 @@
-import {createTodo} from '../apis/Todo';
+import {useEffect} from 'react';
+import TodoController from '../components/Todo/TodoController';
 import TodoCreate from '../components/Todo/TodoCreate';
 
 import * as S from '../styles/Todo.styled';
+import {useTodoState} from '../contexts/TodoContext';
 
 const TodoContainer = () => {
-    const createTodoa = async (todo: string) => {
-        const res = await createTodo({todo});
-        return res;
-    };
+    const todoState = useTodoState();
+    console.info(todoState);
+    const {getTodo, createTodo} = TodoController();
+
+    useEffect(() => {
+        getTodo();
+    }, [getTodo]);
 
     return (
         <S.TodoContainerStyled>
-            <TodoCreate createTodo={createTodoa} />
+            <TodoCreate createTodo={createTodo} />
         </S.TodoContainerStyled>
     );
 };
