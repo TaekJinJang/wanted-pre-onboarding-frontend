e4e4f6e75fddfd707f8cd48f6604a0210b637082
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 15:28:37 +0900

    feat : Router 내장함수 loader를 통해 redirect 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import ROUTES from './constants/routes';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
+import {redirectSignin, redirectTodo} from './utils/redirect';
 
 export const Router = createBrowserRouter([
     {
@@ -12,19 +13,23 @@ export const Router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Navigate to={ROUTES.SIGNIN} />,
+                element: <Navigate to={ROUTES.TODO} replace={true} />,
+                loader: redirectTodo,
             },
             {
                 path: ROUTES.SIGNIN,
                 element: <SignIn />,
+                loader: redirectTodo,
             },
             {
                 path: ROUTES.SIGNUP,
                 element: <SignUp />,
+                loader: redirectTodo,
             },
             {
                 path: ROUTES.TODO,
                 element: <Todo />,
+                loader: redirectSignin,
             },
         ],
     },
