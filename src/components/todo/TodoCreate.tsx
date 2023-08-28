24b6bcda83590cac292cffa2cb39853c1f222eb8
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 21:49:39 +0900

    feat: TODO get,create 적용

--- src/components/todo/TodoCreate.tsx
+++ src/components/todo/TodoCreate.tsx
@@ -2,13 +2,19 @@ import {useRef} from 'react';
 import * as S from '../../styles/Todo.styled';
 import Button from '../common/Button';
 
-const TodoCreate = () => {
+const TodoCreate = ({createTodo}) => {
     // useState를 사용하지않고 Ref를 사용하는 이유는 ?
     // Ref는 값이 변해도 렌더링이 되지 않음.
     // 추가버튼을 클릭했을 때 렌더링이 되며 inputRef의 값이 API로 전달
     const inputRef = useRef<HTMLInputElement | null>(null);
+    const handleFormSubmit = (e: React.FormEvent) => {
+        const value = inputRef.current?.value || null;
+        if (value) {
+            createTodo(value);
+        }
+    };
     return (
-        <S.TodoCreateFormStyled onSubmit={() => console.info('create제출')}>
+        <S.TodoCreateFormStyled onSubmit={handleFormSubmit}>
             {' '}
             <input
                 placeholder='할 일을 입력해주세요... '
--- src/containers/TodoContainer.tsx
+++ src/containers/TodoContainer.tsx
@@ -1,10 +1,18 @@
+import {createTodo, getTodos} from '../apis/Todo';
 import TodoCreate from '../components/Todo/TodoCreate';
 import * as S from '../styles/Todo.styled';
 
 const TodoContainer = () => {
+    const getTodo = getTodos();
+    console.info(getTodo);
+    const createTodoa = async (todo: string) => {
+        const res = await createTodo({todo});
+        return res;
+    };
+
     return (
         <S.TodoContainerStyled>
-            <TodoCreate />
+            <TodoCreate createTodo={createTodoa} />
         </S.TodoContainerStyled>
     );
 };
