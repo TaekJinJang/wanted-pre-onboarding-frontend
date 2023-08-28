f8e794897a35a7c615ae8e7fa0e8bb41ab10155b
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 22:59:22 +0900

    feat : TodoContextAPI 작성

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -18,7 +18,24 @@ const TodoStateContext = createContext<TodoType.Item[] | undefined>(undefined);
 type TodoDispatch = Dispatch<TodoAction>;
 const TodoDispatchContext = createContext<TodoDispatch | undefined>(undefined);
 
-const todoReducer = () => {};
+const todoReducer = (todos: TodoType.Item[], action: TodoAction): TodoType.Item[] => {
+    switch (action.type) {
+        case 'GET':
+            return action.payload;
+        case 'CREATE':
+            return [...todos, action.payload];
+        case 'DELETE':
+            return todos.filter(todo => todo.id !== action.payload);
+        case 'UPDATE':
+            return todos.map(todo =>
+                todo.id === action.payload.id
+                    ? {...todo, todo: action.payload.todo, isCompleted: action.payload.isCompleted}
+                    : todo
+            );
+        default:
+            return todos;
+    }
+};
 
 export const TodoProvider = ({children}: TodoProviderProps) => {
     const [todoState, dispatch] = useReducer(todoReducer, initialState);
@@ -28,4 +45,16 @@ export const TodoProvider = ({children}: TodoProviderProps) => {
             <TodoStateContext.Provider value={todoState}>{children}</TodoStateContext.Provider>
         </TodoDispatchContext.Provider>
     );
+};
+
+export const useTodoState = () => {
+    const state = useContext(TodoStateContext);
+    if (!state) throw new Error('TodoProvider not found');
+    return state;
+};
+
+export const useTodoDispatch = () => {
+    const dispatch = useContext(TodoDispatchContext);
+    if (!dispatch) throw new Error('TodoProvider not found');
+    return dispatch;
 };
