ff7ebcaae6e678b5722ddfbc7d36afb4cca4aa68
Taekjin Jang <xorwls1500@naver.com>
2023-08-28 21:52:03 +0900

    feat : 버튼 클릭시 새로고침 안되게 수정 (e.preventDefault)

--- src/components/todo/TodoCreate.tsx
+++ src/components/todo/TodoCreate.tsx
@@ -2,12 +2,17 @@ import {useRef} from 'react';
 import * as S from '../../styles/Todo.styled';
 import Button from '../common/Button';
 
-const TodoCreate = ({createTodo}) => {
+interface TodoCreateProps {
+    createTodo: (value: string) => void;
+}
+
+const TodoCreate = ({createTodo}: TodoCreateProps) => {
     // useState를 사용하지않고 Ref를 사용하는 이유는 ?
     // Ref는 값이 변해도 렌더링이 되지 않음.
     // 추가버튼을 클릭했을 때 렌더링이 되며 inputRef의 값이 API로 전달
     const inputRef = useRef<HTMLInputElement | null>(null);
     const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
         const value = inputRef.current?.value || null;
         if (value) {
             createTodo(value);
