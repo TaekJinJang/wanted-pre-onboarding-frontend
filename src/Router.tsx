4d338d09056e9533e4f57e8e4cf9c0f017dacb30
Taekjin Jang <xorwls1500@naver.com>
2023-09-14 17:03:46 +0900

    feat: 에러페이지 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
 import {redirectSignin, redirectTodo} from './utils/redirect';
+import NotFound from './pages/NotFound';
 
 export const Router = createBrowserRouter([
     {
@@ -32,5 +33,6 @@ export const Router = createBrowserRouter([
                 loader: redirectSignin,
             },
         ],
+        errorElement: <NotFound />,
     },
 ]);
